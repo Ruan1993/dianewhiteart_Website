ee23aeceeb6b306ea40bd487d920c2a2350805dd
OpenAI <openai@example.com>
2026-04-02 15:34:42 +0200

    Ensure firebase-config.js is committed for frontend runtime

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+window.DWA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDPdzFzDOXWkjZK87maQn0U4bQ_7bDaUIY",
+  authDomain: "diane-white-art.firebaseapp.com",
+  projectId: "diane-white-art",
+  storageBucket: "diane-white-art.firebasestorage.app",
+  messagingSenderId: "121072084384",
+  appId: "1:121072084384:web:34dbcafb2dd0f16ad070c5"
+};
